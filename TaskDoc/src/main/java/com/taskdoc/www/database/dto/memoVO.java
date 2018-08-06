57f6478fcc55a8a996d3562dc4ab766a00bef363
WonDongHo <WonDongHo@DESKTOP-5E574AQ>
2018-08-06 17:57:27 +0900

    dto

--- TaskDoc/src/main/java/com/taskdoc/www/database/dto/memoVO.java
+++ TaskDoc/src/main/java/com/taskdoc/www/database/dto/memoVO.java
@@ -8,4 +8,28 @@ public class memoVO {
 	private int ptcode;
 	private String mcontents;
 	private String mdate;
+	public int getMcode() {
+		return mcode;
+	}
+	public void setMcode(int mcode) {
+		this.mcode = mcode;
+	}
+	public int getPtcode() {
+		return ptcode;
+	}
+	public void setPtcode(int ptcode) {
+		this.ptcode = ptcode;
+	}
+	public String getMcontents() {
+		return mcontents;
+	}
+	public void setMcontents(String mcontents) {
+		this.mcontents = mcontents;
+	}
+	public String getMdate() {
+		return mdate;
+	}
+	public void setMdate(String mdate) {
+		this.mdate = mdate;
+	}
 }
--- TaskDoc/src/main/java/com/taskdoc/www/database/dto/noticeVO.java
+++ TaskDoc/src/main/java/com/taskdoc/www/database/dto/noticeVO.java
@@ -1,5 +1,35 @@
 package com.taskdoc.www.database.dto;
 
-public class noticeVO {
+import org.apache.ibatis.type.Alias;
 
+@Alias("notice")
+public class noticeVO {
+	private int pcode;
+	private String ntitle;
+	private String ncontents;
+	private String ndate;
+	public int getPcode() {
+		return pcode;
+	}
+	public void setPcode(int pcode) {
+		this.pcode = pcode;
+	}
+	public String getNtitle() {
+		return ntitle;
+	}
+	public void setNtitle(String ntitle) {
+		this.ntitle = ntitle;
+	}
+	public String getNcontents() {
+		return ncontents;
+	}
+	public void setNcontents(String ncontents) {
+		this.ncontents = ncontents;
+	}
+	public String getNdate() {
+		return ndate;
+	}
+	public void setNdate(String ndate) {
+		this.ndate = ndate;
+	}
 }
