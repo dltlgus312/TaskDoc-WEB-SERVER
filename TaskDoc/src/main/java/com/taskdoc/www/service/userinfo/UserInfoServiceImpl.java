5e70ea693d9290897b194b93c8003d77d63fe073
WonDongHo <WonDongHo@DESKTOP-5E574AQ>
2018-08-14 20:28:21 +0900

    login passwd check edit

--- TaskDoc/src/main/java/com/taskdoc/www/service/userinfo/UserInfoServiceImpl.java
+++ TaskDoc/src/main/java/com/taskdoc/www/service/userinfo/UserInfoServiceImpl.java
@@ -7,7 +7,7 @@ import com.taskdoc.www.database.dao.userinfo.UserInfoDAO;
 import com.taskdoc.www.database.dto.UserInfoVO;
 
 @Service("UserInfoService")
-public class UserInfoServiceImpl implements UserInfoService{
+public class UserInfoServiceImpl implements UserInfoService {
 
 	@Autowired
 	UserInfoDAO dao;
@@ -17,25 +17,24 @@ public class UserInfoServiceImpl implements UserInfoService{
 		// TODO Auto-generated method stub
 		return dao.userInfoView(userId);
 	}
-	
+
 	@Override
 	public int loginUser(UserInfoVO userInfoVo) {
 		// TODO Auto-generated method stub
-		
+
 		UserInfoVO storageUserInfo = dao.userInfoView(userInfoVo.getUid());
-		
-		if(storageUserInfo == null) {
+
+		if (storageUserInfo == null) {
 			// ID is Null
 			return -1;
-		}
-		else if(storageUserInfo.getUpasswd() != userInfoVo.getUpasswd()) {
+		} else if (!storageUserInfo.getUpasswd().equals(userInfoVo.getUpasswd())) {
 			// PW check Fail
 			return -2;
 		}
-		
+
 		return 1;
 	}
-	
+
 	@Override
 	public int insertUserInfo(UserInfoVO userInfoVo) {
 		// TODO Auto-generated method stub
